38a634ada7444c30e90aaab5407e2d2bb0c65407
StillBlack <stillblack2014@gmail.com>
2025-12-02 15:57:58 +0500

    updated color

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 207, 211, 213),
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(175, 227, 217, 179),
+          backgroundColor: const Color.fromARGB(174, 232, 189, 20),
           title: Center(child: Text('I Am Poor')),
         ),
         body: Center(child: Image(image: AssetImage('images/poor.jpg'))),
