2ff81af51f719366b7ffa126fe54c3c23a0d63cd
StillBlack <stillblack2014@gmail.com>
2025-12-02 15:55:12 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 207, 211, 213),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(175, 227, 217, 179),
+          title: Center(child: Text('I Am Poor')),
+        ),
+        body: Center(child: Image(image: AssetImage('images/poor.jpg'))),
+      ),
+    ),
+  );
+}
